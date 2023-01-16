7fbdfc6fec92bfd944357f879cd933038a4b74f2
Mark Bersalona <mark@bersalona.com>
2023-01-16 16:23:08 -0500

    Copy/edit NetBeans-generated makefiles
    Copy and edit the NetBeans-generated makefiles from Stratus Display

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=400_cellular_diagnostic
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/400_cellular_diagnostic
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=rackmounteddisplay.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/400_cellular_diagnostic.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=rack_mounted_display
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/400_cellular_diagnostic
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=400_cellular_diagnostic.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/400_cellular_diagnostic.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
